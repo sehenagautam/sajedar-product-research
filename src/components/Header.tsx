b88009aa146847bc9c2e5e7f334b380597fa1edd
dimanjan <dimanjandahal@gmail.com>
2025-10-20 00:46:17 +0545

    Update logo to sajedar-new-logo.png across header, favicon, and metadata

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
           <Link href="/" className="flex items-center space-x-3 hover:opacity-80 transition-opacity duration-200">
             <div className="relative w-10 h-10">
               <Image
-                src="/sajedar-logo.png"
+                src="/sajedar-new-logo.png"
                 alt="Sajedar Logo"
                 fill
                 sizes="40px"
